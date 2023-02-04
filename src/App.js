ff9277cc3125c9db5ada2278b4f91d21933b05f1
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-04 20:34:28 +0530

    Displaying the articles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [newsList, setNewsList] = useState([]);
+
   useEffect(() => {
     fetch('https://api.spaceflightnewsapi.net/v3/articles')
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => setNewsList(data))
       .catch((error) => console.log(error));
   });
+
   return (
     <div className="App">
       <div className="title">
         <h1>Space News</h1>
       </div>
-      <div className="newsContainer"></div>
+      <div className="newsContainer">
+        {newsList.map((value, key) => {
+          return (
+            <div key={key}>
+              <h3>{value.title}</h3>
+              <img src={value.imageUrl} alt={value.id} />
+              <p>{value.summary}</p>
+              <h4>{value.publishedAt}</h4>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
